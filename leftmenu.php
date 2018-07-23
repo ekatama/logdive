ef91b4769416a405e31cf3e7320f0281d239a00b
unknown <alrizki.istighfar@gmail.com>
2018-07-23 08:32:41 +0700

    Layout Design

---
+++ leftmenu.php
@@ -0,0 +1,133 @@
+<aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu" id="nav-accordion">
+              
+              	  <?php
+                    if($halaman=='dashboard'){
+                  ?>
+                  <li>
+                      <a class="active" href="dashboard.php">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+                  <?php
+                    }
+                    else {
+                  ?>
+                      <li>
+                      <a href="dashboard.php">
+                          <i class="fa fa-dashboard"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+                  <?php
+                    }
+                  ?>
+
+                   <?php
+                    if($halaman=='news'){
+                  ?>
+                  <li>
+                      <a class="active" href="news.php">
+                          <i class="fa fa-calendar"></i>
+                          <span>News</span>
+                      </a>
+                  </li>
+                  <?php
+                    }
+                    else {
+                  ?>
+                      <li>
+                      <a href="news.php">
+                          <i class="fa fa-calendar"></i>
+                          <span>News</span>
+                      </a>
+                  </li>
+                  <?php
+                    }
+                  ?>
+
+                  <?php
+                    if($halaman=='divecenter'){
+                  ?>
+                  <li class="sub-menu">
+                      <a class="active" href="javascript:;" >
+                          <i class="fa fa-anchor"></i>
+                          <span>Dive Center</span>
+                      </a>
+                      <ul class="sub">
+                          <li class="active"><a  href="divecenter.php">Dive Center</a></li>
+                          <li><a  href="gallery.html">Gallery</a></li>
+                          <li><a  href="todo_list.html">Todo List</a></li>
+                      </ul>
+                  </li>
+<?php
+}
+else{
+?>
+<li class="sub-menu">
+                      <a href="javascript:;" >
+                          <i class="fa fa-anchor"></i>
+                          <span>Dive Center</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="divecenter.php">Dive Center</a></li>
+                          <li><a  href="gallery.html">Gallery</a></li>
+                          <li><a  href="todo_list.html">Todo List</a></li>
+                      </ul>
+                  </li>
+<?php
+}
+?>
+                  <?php
+                      if($halaman=='batee_tokong'){
+                  ?>
+                  <li class="sub-menu">
+                  
+                      <a class="active" href="javascript:;" >
+                          <i class="fa fa-globe"></i>
+                          <span>Dive Site</span>
+                      </a>
+                      <ul class="sub">
+                          <li class="active"><a  href="batee_tokong.php">Batee Tokong (2)</a></li>
+                          <li><a  href="login.html">Pulau Seulako (3)</a></li>
+                          
+                      </ul>
+                  </li>
+                  <?php
+                    }
+                    else {
+                  ?>
+
+                      <li class="sub-menu">
+                  
+                      <a href="javascript:;" >
+                          <i class="fa fa-globe"></i>
+                          <span>Dive Site</span>
+                      </a>
+                      <ul class="sub">
+                          <li><a  href="batee_tokong.php">Batee Tokong (2)</a></li>
+                          <li><a  href="login.html">Pulau Seulako (3)</a></li>
+                          
+                      </ul>
+                  </li>
+
+                  <?php 
+                }
+
+                  ?>
+                  <li class="sub-menu">
+                      <a href="login.php" >
+                          <i class="fa fa-book"></i>
+                          <span>Log Dive</span>
+                      </a>
+                      
+                  </li>
+                
+
+              </ul>
+              <!-- sidebar menu end-->
+          </div>
+      </aside>
